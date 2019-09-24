8abb18f868298083c164933af0d30b3b8d8a0b4b
Meiqi-Wu <meiqiwu9878@gmail.com>
2019-09-23 21:27:14 -0400

    visualization and LSTM

---
+++ data-visualization.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 23 21:13:44 2019
+
+@author: wumeiqi
+"""
+
+import seaborn as sns
+import matplotlib.pyplot as plt
---
+++ senti-analysis.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 23 21:15:12 2019
+
+@author: wumeiqi
+"""
+
+from keras.preprocessing.text import Tokenizer
+
+
+max_features = 1000
